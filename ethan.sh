eddac4c2987107da6b386a1e8355bece8de851d6
TheLinuxProject <64985848+thelinuxproject@users.noreply.github.com>
2020-06-14 00:21:32 +0200

    Delete ethan.sh

--- ethan.sh
+++
@@ -1,9 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-apt update && apt full-upgrade -y
-apt install wget zip -y
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip -O ~/ngrok.zip
-unzip ~/ngrok.zip -d $PREFIX/bin
-rm -rf ~/ngrok.zip
-
-ngrok
